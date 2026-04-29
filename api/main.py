aa83705ffd935ee5f97411c2237b7de231c9fa3e
susmithays <167988307+susmithays@users.noreply.github.com>
2026-04-29 16:40:54 +0100

    Update main.py

--- api/main.py
+++ api/main.py
@@ -6,7 +6,7 @@ import os
 # ---------------------------
 # CHANGE THESE IF NEEDED
 # ---------------------------
-S3_BUCKET = "loksai-edu-mlproject1"
+S3_BUCKET = "mlopsyan-bucket1"
 S3_KEY = "latest/model.pkl"
 LOCAL_MODEL_PATH = "models/model.pkl"
 # ---------------------------
